279f68a452c77416a61f73042f54702d794692f1
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-06-22 17:15:56 +0000

    stat test

--- app/Pages/AllHabits/Components/AllHabitsRightSideBar.tsx
+++ app/Pages/AllHabits/Components/AllHabitsRightSideBar.tsx
@@ -1,7 +1,15 @@
 import React from "react"
 
+import { defaultColor } from "@/colors";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import UserProfile from "./RightSideBar/UserProfile"
+import MainStatistics from "./RightSideBar/MainStatistics"
+
 function AllHabitsRightSideBar() {
-    return <div className = " w-[20%] bg-white "> </div>
+    return <div className = " w-[30%] flex flex-col items-center bg-white ">
+        <UserProfile />
+        <MainStatistics />
+    </div>
 }
 
 export default AllHabitsRightSideBar;
