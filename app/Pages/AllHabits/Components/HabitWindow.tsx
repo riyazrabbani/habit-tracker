c48f3a809140df258255f4358329d144cf19a0f7
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-06-23 23:06:58 +0000

    added icon functionality

--- app/Pages/AllHabits/Components/HabitWindow.tsx
+++ app/Pages/AllHabits/Components/HabitWindow.tsx
@@ -3,17 +3,20 @@
 import { useGlobalContextProvider } from "@/app/contextApi"
 import { darkModeColor, defaultColor } from "@/colors"
 import { faArrowAltCircleDown } from "@fortawesome/free-regular-svg-icons"
-import { faC, faChevronDown, faClose } from "@fortawesome/free-solid-svg-icons"
+import { faStar, faChevronDown, faClose, faQuestion } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { memo, useRef, useEffect, useState } from "react";
+import IconsWindow from "./IconsWindow/IconsWindow";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 type HabitType = {
     _id: string;
     name: string;
+    icon: IconProp;
 }
 
-const HeaderMemo = memo(Header);
-const InputNameAndIconButtonMemo = memo(InputNameAndIconButton);
+// const HeaderMemo = memo(Header);
+// const InputNameAndIconButtonMemo = memo(InputNameAndIconButton);
 
 function HabitWindow() {
     const { habitWindowObject, darkModeObject } = useGlobalContextProvider();
@@ -22,13 +25,22 @@ function HabitWindow() {
     const [habitItem, setHabitItem] = useState<HabitType>({
         _id: "",
         name: "",
+        icon: faQuestion,
     });
+    const [openIconWindow, setOpenIconWindow] = useState<boolean>(false);
+    const [iconSelected, setIconSelected] = useState<IconProp>(habitItem.icon);
 
     const onUpdateHabitName = (inputText: string) => {
         const copyHabitItem = { ...habitItem };
         copyHabitItem.name = inputText;
         setHabitItem(copyHabitItem);
     };
+
+    useEffect(() => {
+        const copyHabitItem = {...habitItem};
+        copyHabitItem.icon = iconSelected;
+        setHabitItem(copyHabitItem);
+    }, [iconSelected]);
 
     return (
         <div
@@ -39,10 +51,18 @@ function HabitWindow() {
             className={` top-[3%] left-1/2 transform -translate-x-1/2 w-[80%] z-50 p-10
                 rounded-md shadow-md ${openHabitWindow ? "absolute" : "hidden"}`}
         >
-            <HeaderMemo />
-            <InputNameAndIconButtonMemo
+            <IconsWindow
+                  openIconWindow = {openIconWindow}
+                  setOpenIconWindow = {setOpenIconWindow}
+                  iconSelected = {iconSelected}
+                  setIconSelected = {setIconSelected}
+            />
+            <Header />
+            <InputNameAndIconButton
                 onUpdateHabitName={onUpdateHabitName}
                 habitName={habitItem.name}
+                setOpenIconWindow = {setOpenIconWindow}
+                iconSelected = {iconSelected}
             />
             <SaveButton habit={habitItem} />
         </div>
@@ -72,9 +92,13 @@ function Header() {
 function InputNameAndIconButton({
     onUpdateHabitName,
     habitName,
+    setOpenIconWindow,
+    iconSelected,
 }: {
     onUpdateHabitName: (inputText: string) => void;
     habitName: string;
+    setOpenIconWindow: React.Dispatch<React.SetStateAction<boolean>>;
+    iconSelected: IconProp;
 }) {
     const inputRef = useRef<HTMLInputElement>(null);
     const { habitWindowObject, darkModeObject } = useGlobalContextProvider();
@@ -95,6 +119,10 @@ function InputNameAndIconButton({
         }
     }, [openHabitWindow]);
 
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [iconSelected]);
+
     return (
         <div className="flex flex-col gap-2 mt-10 px-3">
             <span className="opacity-80 font-semibold">Habit Name</span>
@@ -110,10 +138,11 @@ function InputNameAndIconButton({
                     className={`border w-full border-gray-200 outline-none p-4 rounded-md text-[13px]`}
                     placeholder="Type a name for the habit..."
                 />
-
+ 
                 <FontAwesomeIcon
+                    onClick = {() => setOpenIconWindow(true)}
                     className="bg-mainColor mt-[1px] p-4 rounded-md text-white cursor-pointer bg-customBlue"
-                    icon={faClose}
+                    icon={iconSelected}
                     height={16}
                     width={20}
                 />
---
+++ app/Pages/AllHabits/Components/IconsWindow/IconData.tsx
@@ -0,0 +1,162 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+import { 
+    faCalculator,
+    faFlask,
+    faGlobe,
+    faBook,
+    faDumbbell,
+    faLaptopCode,
+    faBookMedical,
+    faPalette,
+    faComments,
+    faPhoneAlt,
+    faEnvelope,
+    faSearch,
+    faMusic,
+    faCamera,
+    faQuestion,
+    faCogs,
+    faUser,
+    faGraduationCap,
+    faUsers,
+    faMoneyBill,
+    faBriefcase,
+    faTools,
+    faLightbulb,
+    faPlaneDeparture,
+    faBasketShopping,
+    faPersonWalking,
+    faBasketball,
+    faCar,
+    faFaceMeh,
+    faFaceGrinStars,
+} from "@fortawesome/free-solid-svg-icons"
+
+type iconData = {
+    faIcon: IconProp;
+    isSelected: boolean;
+}
+
+export const iconsData: iconData[] = [
+    {
+        faIcon: faCalculator,
+        isSelected: true,
+    },
+    {
+        faIcon: faFlask,
+        isSelected: false,
+    },
+    {
+        faIcon: faGlobe,
+        isSelected: false,
+    },
+    {
+        faIcon: faBook,
+        isSelected: false,
+    },
+    {
+        faIcon: faDumbbell,
+        isSelected: false,
+    },
+    {
+        faIcon: faLaptopCode,
+        isSelected: false,
+    },
+    {
+        faIcon: faBookMedical,
+        isSelected: false,
+    },
+    {
+        faIcon: faPalette,
+        isSelected: false,
+    },
+    {
+        faIcon: faComments,
+        isSelected: false,
+    },
+    {
+        faIcon: faPhoneAlt,
+        isSelected: false,
+    },
+    {
+        faIcon: faEnvelope,
+        isSelected: false,
+    },
+    {
+        faIcon: faSearch,
+        isSelected: false,
+    },
+    {
+        faIcon: faMusic,
+        isSelected: false,
+    },
+    {
+        faIcon: faCamera,
+        isSelected: false,
+    },
+    {
+        faIcon: faQuestion,
+        isSelected: false,
+    },
+    {
+        faIcon: faCogs,
+        isSelected: false,
+    },
+    {
+        faIcon: faUser,
+        isSelected: false,
+    },
+    {
+        faIcon: faGraduationCap,
+        isSelected: false,
+    },
+    {
+        faIcon: faUsers,
+        isSelected: false,
+    },
+    {
+        faIcon: faMoneyBill,
+        isSelected: false,
+    },
+    {
+        faIcon: faBriefcase,
+        isSelected: false,
+    },
+    {
+        faIcon: faTools,
+        isSelected: false,
+    },
+    {
+        faIcon: faLightbulb,
+        isSelected: false,
+    },
+    {
+        faIcon: faPlaneDeparture,
+        isSelected: false,
+    },
+    {
+        faIcon: faBasketShopping,
+        isSelected: false,
+    },
+    {
+        faIcon: faPersonWalking,
+        isSelected: false,
+    },
+    {
+        faIcon: faBasketball,
+        isSelected: false,
+    },
+    {
+        faIcon: faCar,
+        isSelected: false,
+    },
+    {
+        faIcon: faFaceGrinStars,
+        isSelected: false,
+    },
+    {
+        faIcon: faFaceMeh,
+        isSelected: false,
+    },
+];
