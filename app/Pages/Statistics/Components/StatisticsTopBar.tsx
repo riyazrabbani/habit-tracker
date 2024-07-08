fde32e33b4244587ca1e87c76acab6ff3c254c2e
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-07-08 00:27:14 +0000

    vercel deployment

--- app/Pages/Statistics/Components/StatisticsTopBar.tsx
+++ app/Pages/Statistics/Components/StatisticsTopBar.tsx
@@ -1,4 +1,4 @@
-import React from "React";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useGlobalContextProvider } from "@/app/contextApi";
